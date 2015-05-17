929291e630ced3961a205aa1e47059bbfe31ce45
George <lovero1989@gmail.com>
2015-05-17 20:49:59 +0800

    add doublebuffer test

---
+++ OPENGL_RED/C1_Introduction_to_OpenGL/DoubleBuffer/DoubleBuffer/double_buffer.cpp
@@ -0,0 +1,106 @@
+#include <Windows.h>
+#include <GL/GL.h>
+#include <gl/GLU.h>
+#include <GL/glut.h>
+
+static GLfloat spin = 0.0f;
+
+void init(void)
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glShadeModel(GL_FLAT);
+}
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
+	glRotated(spin, 0.0, 0.0, 1.0f);
+	glColor3f(1.0, 1.0f, 0.0);
+	glRectf(-25.0, -25.0, 25.0, 25.0);
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+void spinDisplay(void)
+{
+	spin += 2.0;
+	if (spin > 360)
+	{
+		spin -= 360;
+	}
+	glutPostRedisplay();
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0f);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	switch(button)
+	{
+	case GLUT_LEFT_BUTTON:
+		if (state == GLUT_DOWN)
+		{
+			glutIdleFunc(spinDisplay);
+		}
+		break;
+	case GLUT_MIDDLE_BUTTON:
+		if (state == GLUT_DOWN)
+		{
+			glutIdleFunc(NULL);
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+int main(int argc, char ** argv)
+{
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+
+	glutInitWindowSize(250, 250);
+	glutInitWindowPosition(50 , 50);
+	
+	glutCreateWindow(argv[0]);
+	
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
+	
+	glutMainLoop();
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
